b6b81104ece70860b799c768f19f9abe5192580e
vista-narvas <vista-narvas@users.noreply.github.com>
2014-11-08 15:11:55 +0000

    Update punyama.sh

--- punyama.sh
+++ punyama.sh
@@ -14,7 +14,7 @@ in=$HOME/.punyama/text/$server/\#$channel/in
 out=$HOME/.punyama/text/$server/\#$channel/out
 
 # Say hi
-echo "leave me out of this~" > $in
+echo "fullhouse~" > $in
 
 tailf -n 1 $out | \
 while read date time nick msg; do
