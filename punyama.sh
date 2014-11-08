d81ea7c9892a7e2b0d090c2c4bcafff9c6fdca69
vista-narvas <vista-narvas@users.noreply.github.com>
2014-11-08 16:23:27 +0000

    Update punyama.sh

--- punyama.sh
+++ punyama.sh
@@ -306,9 +306,7 @@ while read date time nick msg; do
 
 		elif [[ $msg == ".time till "* ]]; then
 			word=$(echo $msg | cut -d " " -f 3-)
-			echo "$(date -d $word +"%s")-$(date +"%s")" > $in
-			echo "$(echo "$(date -d $word +"%s")-$(date +"%s")"|bc)" > $in
-			# echo "$(echo "$(date -d $word +"%s")-$(date +"%s")" | bc) seconds till $word" > in
+			echo "$(echo "$(date -d $word +"%s")-$(date +"%s")"|bc) seconds till $word" > $in
 
 		# Check time
 		# TODO: Add betime thingy
