f51738c136fb67317c9eba265c91e6af19d2bf56
vista-narvas <vista-narvas@users.noreply.github.com>
2014-11-08 16:20:03 +0000

    Update punyama.sh

--- punyama.sh
+++ punyama.sh
@@ -306,9 +306,9 @@ while read date time nick msg; do
 
 		elif [[ $msg == ".time till "* ]]; then
 			word=$(echo $msg | cut -d " " -f 3-)
-			echo "$word" > $in
-			time1 = date -d $word +"%s"
-			time2 = date +"%s"
+			# echo "$word" > $in
+			time1 = $(date -d $word +"%s")
+			time2 = $(date +"%s")
 			echo "$time1 $time2" > $in
 			echo "$(echo "$(date -d $word +"%s")-$(date +"%s")" | bc) seconds till $word" > in
 
