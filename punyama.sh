fff778d77f5660ab700b9776fdf6099993b5b756
vista-narvas <vista-narvas@users.noreply.github.com>
2014-11-08 15:08:41 +0000

    Update punyama.sh

--- punyama.sh
+++ punyama.sh
@@ -14,7 +14,7 @@ in=$HOME/.punyama/text/$server/\#$channel/in
 out=$HOME/.punyama/text/$server/\#$channel/out
 
 # Say hi
-echo "ReportingReporting inin~" > $in
+echo "leave me out of this~" > $in
 
 tailf -n 1 $out | \
 while read date time nick msg; do
