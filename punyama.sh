ff288130b17509056d03f9ef8563004249732897
vista-narvas <vista-narvas@users.noreply.github.com>
2014-11-17 21:34:07 +0100

    remove .help debug
    its working now

--- punyama.sh
+++ punyama.sh
@@ -75,8 +75,6 @@ while read date time nick msg; do
 		# Display help
 		if [[ $msg == ".help"* ]]; then
 			word=$(echo $msg | sed 's/^\.help //' )
-			echo $msg > $in
-			echo $word > $in
 			case "$word" in
 				about)
 					echo "info about .about" > $in
